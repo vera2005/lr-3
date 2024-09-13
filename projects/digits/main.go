cf215c6439f6ba71330260973a13036055a52289
Vera <78332987+vera2005@users.noreply.github.com>
2024-09-13 18:48:16 +0300

    Update main.go

--- projects/digits/main.go
+++ projects/digits/main.go
@@ -3,5 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, world!!!")
+	ans := 0
+	var a string
+	fmt.Scan(&a)
+	srezRune := []rune(a)
+	for _, el := range srezRune {
+		if int(el) > ans {
+			ans = int(el)
+		}
+	}
+	fmt.Print(string(ans))
 }
