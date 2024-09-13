d678cd7c5806ae189d40d0271e2852b9cbe96839
Vera <78332987+vera2005@users.noreply.github.com>
2024-09-13 17:31:43 +0300

    Update main.go

--- projects/atoi/main.go
+++ projects/atoi/main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Hello, world!!!")
+	var a int
+	i := 0
+	fmt.Scan(&a)
+	preAns := make([]int, 0, 0)
+	for a > 0 {
+		preAns = append(preAns, (a%10)*(a%10))
+		a /= 10
+		i += 1
+	}
+	ans := 0
+	for i-1 >= 0 {
+		q := preAns[i-1]
+		if q > 10 {
+			ans = ans*100 + q
+		} else {
+			ans = ans*10 + q
+		}
+		i -= 1
+	}
+	fmt.Print(ans)
 }
+
+
+
+
+
