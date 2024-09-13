d64c4ed72e38d0553d82228e7c5d95e93042b53b
Vera <78332987+vera2005@users.noreply.github.com>
2024-09-13 18:53:45 +0300

    Update main.go

--- projects/star/main.go
+++ projects/star/main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+	"unicode/utf8"
+)
 
 func main() {
-	fmt.Println("Hello, world!!!")
+	var a string
+	fmt.Scan(&a)
+	qwe := make([]string, 0, 0)
+	srOfRune := []rune(a)
+	for i := range srOfRune {
+		if i != utf8.RuneCountInString(a)-1 {
+			qwe = append(qwe, string(srOfRune[i]), "*")
+		} else {
+			qwe = append(qwe, string(srOfRune[i]))
+		}
+	}
+	ans := strings.Join(qwe, "")
+	fmt.Print(ans)
+
 }
